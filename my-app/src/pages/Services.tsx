4f35a387b364cc024b1c986fa46dabdd20a9f8e6
AliseToca <at00687@rvt.lv>
2025-09-17 15:11:57 +0300

    Added component and made loading, error states

--- my-app/src/pages/Services.tsx
+++ my-app/src/pages/Services.tsx
@@ -3,6 +3,7 @@ import { Box, Grid, Typography, CircularProgress, Alert, TextField } from "@mui/
 import SearchBar from "../components/SearchBar";
 import { useAppDispatch, useAppSelector } from "../store/hooks";
 import { fetchServices } from "../store/servicesSlice"; // <-- async thunk
+import ServiceCard from "../components/ServiceCard";
 
 function ServicesPage() {
   const dispatch = useAppDispatch();
@@ -27,18 +28,15 @@ function ServicesPage() {
     filteredServices = filteredServices.sort((a, b) => a.price - b.price);
   } else if (sortBy === "title") {
     filteredServices = filteredServices.sort((a, b) =>
-        a.title.localeCompare(b.title)
+      a.title.localeCompare(b.title)
     );
   }
 
   return (
     <Box sx={{ p: { xs: 2, md: 6 } }}>
-      <Typography variant="h3" gutterBottom>
-        Our Services
-      </Typography>
 
-      {/* Search bar */}
-      <Box sx={{ mb: 4 }}>
+      {/*--Top actions--*/}
+      <Box sx={{ display: "flex", gap: 4, mb: 4 }}>
         <SearchBar
           value={query}
           onChange={setQuery}
@@ -47,10 +45,16 @@ function ServicesPage() {
 
         <TextField
           select
-          label="Sort by"
           value={sortBy || ""}
           onChange={(e) => setSortBy(e.target.value as "title" | "price" | null)}
-          SelectProps={{ native: true }}
+          slotProps={{
+            select: {
+              native: true,
+            },
+          }}
+          sx={{
+            width: "10rem"
+          }}
         >
           <option value="">None</option>
           <option value="title">Title</option>
@@ -58,57 +62,68 @@ function ServicesPage() {
         </TextField>
       </Box>
 
-      {loading && <CircularProgress />}
-      {error && <Alert severity="error">{error}</Alert>}
+      {/*---Loading screen--*/}
+      {loading && (
+        <Box
+          sx={{
+            minHeight: "70vh", 
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <CircularProgress />
+        </Box>
+      )}
+      
+      {/*---Error screen---*/}
+      {error && (
+        <Box
+          sx={{
+            minHeight: "80vh", 
+          }}
+        >
+          <Alert
+            severity="error"
+            sx={{
+              width: "100%",
+              textAlign: "center",
+              p: 1,
+              borderRadius: 2,
+              fontSize: "1rem",
+            }}
+          >
+            {error}
+          </Alert>
+        </Box>
+      )}
 
-      <Grid container spacing={4}>
-        {filteredServices.length === 0 && !loading ? (
-          <Typography variant="body1" sx={{ p: 2 }}>
-            No services found.
+      {/*---No results on search---*/}
+      {!loading && !error && filteredServices.length === 0 && (
+        <Box
+          sx={{
+            minHeight: "80vh",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Typography variant="h6" color="text.secondary">
+            No results found.
           </Typography>
-        ) : (
-          filteredServices.map((service) => (
+        </Box>
+      )}
+
+      {/*---Service cards---*/}
+      {!loading && !error && filteredServices.length > 0 && (
+        <Grid container spacing={4}>
+          {filteredServices.map((service) => (
             <Grid key={service.id} size={{xs:12, sm:6, md:4}}>
-             <Box
-                sx={{
-                  p: 2,
-                  border: "1px solid #ddd",
-                  borderRadius: 2,
-                  boxShadow: 1,
-                  textAlign: "center",
-                }}
-              >
-                {service.image && (
-                  <Box
-                    component="img"
-                    src={service.image}
-                    alt={service.title}
-                    sx={{
-                      width: "100%",
-                      height: 180,
-                      objectFit: "cover",
-                      borderRadius: 1,
-                      mb: 2,
-                    }}
-                  />
-                )}
-                <Typography variant="h6" gutterBottom>
-                  {service.title}
-                </Typography>
-                <Typography variant="body2" color="text.secondary" sx={{ mb: 1 }}>
-                  {service.description}
-                </Typography>
-                <Typography variant="body2" sx={{ fontStyle: "italic", mb: 1 }}>
-                  Category: {service.category}
-                </Typography>
-                <Typography variant="subtitle1" fontWeight="bold">
-                  ${service.price}
-                </Typography>
-              </Box>
+              <ServiceCard service={service} />
             </Grid>
-          ))
+          ))}
+        </Grid>
         )}
-      </Grid>
     </Box>
   );
 }
